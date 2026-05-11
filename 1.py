513963976248fab0b1914e727c1a50b0fe0ad279
yael200206 <hiram060220@gmail.com>
2026-05-11 13:15:44 -0600

    1

---
+++ 1.py
@@ -0,0 +1,12 @@
+calificacion=int(input("ingrese la calificaciuon"))
+#calificacion es la variable a analizar , la transformamos a un numero entero para redondear
+#y para darle un valor a esta varibale sirve la funcion input que sirve para ingresar datos desde termianl
+#
+
+if calificacion >= 7:# en esta linea se hace la comparacion con el >= que es para ver si es mayor o igual a 7 osea 8,9,10
+    #aqui se imprime el resultado
+    print("buena perro eres bueno y pasaste")
+else :#esta es la funcion para la condicional , si no es >= a 7 hace lo siguiente
+
+    # se imprime el resultado esperado si el resultado no es el esperado
+    print("burro de kk reprobaste vas a recursamiento ")
